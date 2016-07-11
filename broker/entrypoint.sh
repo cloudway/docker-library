9a34a6d2512e2c89184503a473fcdd1755deb97c
daniel <daniel.yuan@me.com>
2016-07-11 14:49:17 +0800

    为broker增加smtp服务

--- broker/entrypoint.sh
+++ broker/entrypoint.sh
@@ -7,6 +7,7 @@ if [ "$CLOUDWAY_DOMAIN" != 'example.com' ]; then
     sed -i "s/example\\.com/$CLOUDWAY_DOMAIN/g" /usr/local/cloudway/conf/cloudway.conf
 fi
 
+# configure bitbucket
 if [ -z "$CLOUDWAY_SCM_URL" -a -n "$BITBUCKET_ENV_BITBUCKET_URL" ]; then
     BITBUCKET_HOST=$BITBUCKET_PORT_7990_TCP_ADDR
     BITBUCKET_PORT=$BITBUCKET_PORT_7990_TCP_PORT
@@ -17,6 +18,48 @@ if [ -z "$CLOUDWAY_SCM_URL" -a -n "$BITBUCKET_ENV_BITBUCKET_URL" ]; then
     export CLOUDWAY_SCM_TYPE=bitbucket
     export CLOUDWAY_SCM_URL=$BITBUCKET_URL
     export CLOUDWAY_SCM_CLONE_URL="git clone ssh://git@git.${CLOUDWAY_DOMAIN}:7999/<namespace>/<repo>.git"
+fi
+
+# configure postfix
+if [ ! -x /usr/local/bin/postfix.sh ]; then
+    postconf -e myhostname=$CLOUDWAY_DOMAIN
+    postconf -e mydestination=$CLOUDWAY_DOMAIN,localhost
+    postconf -e smtpd_sasl_auth_enable=yes
+    postconf -e broken_sasl_auth_clients=yes
+    postconf -e smtpd_recipient_restrictions=permit_sasl_authenticated,reject_unauth_destination
+    postconf -e smtpd_use_tls=no
+    postconf -e inet_interfaces=127.0.0.1
+    postconf -F '*/*/chroot = n'
+
+    cat >> /etc/postfix/sasl/smtpd.conf <<EOF
+pwcheck_method: auxprop
+auxprop_plugin: sasldb
+mech_list: PLAIN LOGIN CRAM-MD5 DIGEST-MD5 NTLM
+EOF
+
+    # generate random password
+    username=postmaster
+    password=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w20 | head -n1)
+    echo $password | saslpasswd2 -p -c -u $CLOUDWAY_DOMAIN $username
+    chown postfix.sasl /etc/sasldb2
+
+    sed -i -e '/^\[smtp\]/,/^\[/{
+      s/#host = .*/host = 127.0.0.1/
+      s/#port = .*/port = 25/
+      s/#username = .*/username = '$username'/
+      s/#password = .*/password = '$password'/
+      s/#from = .*/from = Cloudway <'$username'@'$CLOUDWAY_DOMAIN'>/
+    }' conf/cloudway.conf
+    chmod 0640 conf/cloudway.conf
+
+    cat > /usr/local/bin/postfix.sh <<EOF
+#!/bin/bash -e
+trap "/usr/sbin/postfix stop" EXIT
+trap "/usr/sbin/postfix reload" SIGHUP
+/usr/sbin/postfix start
+sleep infinity
+EOF
+    chmod +x /usr/local/bin/postfix.sh
 fi
 
 exec supervisord -c /etc/supervisor/supervisord.conf
